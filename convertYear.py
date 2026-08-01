eede858478ccaa2db6b9b991ac440226057f10e3
411-07-Wilasinee <54241@yupparaj.ac.th>
2026-08-01 11:19:37 +0700

    Fix input range for year conversion

--- convertYear.py
+++ convertYear.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ.")
 
-bh_year-st.number_input("กรอกปี พ.ศ.:", value=1)
+bh_year-st.number_input("กรอกปี พ.ศ.:", value=1 min=1 max=2569)
 ce_year=bh_year-543
 st.header ("ปี ค.ศ. คือ : f{ce_year}")
