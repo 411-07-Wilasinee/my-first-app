90b1618d17f4355ed395d441b9ce9b14e372f1a3
411-07-Wilasinee <54241@yupparaj.ac.th>
2026-08-20 08:24:32 +0700

    Fix variable name for weight input in bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -8,7 +8,7 @@ st.write("กรอกข้อมูลน้ำหนักและส่ว�
 
 #ส่วนที่ 2 สร้างช่องรับค่าน้ำหนัก และ ส่วนสูง
 
-weight st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
+weight_st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
 
 height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร) :", min_value=1.0, value=1.0)
 
